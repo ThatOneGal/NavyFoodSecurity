7a5ad47a1bc6fab860b2cc1ca70b2eaacdbb27d2
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-07-16 22:37:42 +0800

    fixed typo in location edit blade file

--- NavyFoodApp/resources/views/Status/edit.blade.php
+++ NavyFoodApp/resources/views/Status/edit.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <form method="POST" action="{{route('status.update', $location)}}">
+                <form method="POST" action="{{route('status.update', $status)}}">
                     @method('PUT')
                     @csrf
 
