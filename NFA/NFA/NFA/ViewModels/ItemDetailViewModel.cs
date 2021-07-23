aa723cd1cb2950f86b9adecc6ddf2c71960402f5
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-07-23 11:25:59 +0800

    created empty Xamirin project
    Tabbed template, includes API

---
+++ NFA/NFA/NFA/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NFA.Models;
+
+namespace NFA.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
