fcf32ccc00881b5d43648f221ad03bbb5b382074
William <69453507+BoxedSnake@users.noreply.github.com>
2021-10-27 08:24:45 +0800

    qr reader test - william

---
+++ ZXingTest/ZXingTest/ZXingTest/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXingTest.Models;
+using ZXingTest.ViewModels;
+using ZXingTest.Views;
+
+namespace ZXingTest.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ ZXingTest/ZXingTest/ZXingTest/Views/NewItemPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ZXingTest.Models;
+using ZXingTest.ViewModels;
+
+namespace ZXingTest.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+
+        private async Task Button_ClickedAsync(object sender, EventArgs e)
+        {
+#if __ANDROID__
+            // Initialize the scanner first so it can track the current context
+            MobileBarcodeScanner.Initialize (Application);
+#endif
+            try
+            {
+
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            var result = await scanner.Scan();
+
+            if (result != null)
+                Console.WriteLine("Scanned Barcode: " + result.Text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
