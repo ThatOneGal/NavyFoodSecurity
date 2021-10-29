09755b31505489e77f3a5d9bb6eb16c6b08f6223
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-10-29 09:28:38 +0800

    added zxing packages, added qr scanner code to app

--- NFA/NFA/NFA/Views/AppDashboardPage.xaml.cs
+++ NFA/NFA/NFA/Views/AppDashboardPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing;
 
 namespace NFA.Views
 {
@@ -15,6 +16,16 @@ namespace NFA.Views
         public AppDashboardPage()
         {
             InitializeComponent();
+        }
+
+        public void scanView_OnScanResult(Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned result", "The barcode's text is " + result.Text + ". The barcode's format is " + result.BarcodeFormat, "OK");
+            });
+
+            //off
         }
     }
 }
