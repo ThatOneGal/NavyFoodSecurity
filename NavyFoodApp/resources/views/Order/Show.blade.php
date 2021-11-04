ff3b644e4505c155da38e1870b17fa20dd873f71
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-11-04 10:27:51 +0800

    Update Show.blade.php

--- NavyFoodApp/resources/views/Order/Show.blade.php
+++ NavyFoodApp/resources/views/Order/Show.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="display:flex; justify-content: center">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="display:flex; align-items: center; justify-content: center">
 
                 <div> {{--Destination--}}
                     <div><label for="Location">Destination:</label></div>
