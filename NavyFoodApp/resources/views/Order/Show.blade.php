41ecd1b31de887e5834fb044aa2d85ad9d50a3f6
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-11-04 10:47:29 +0800

    Update Show.blade.php

--- NavyFoodApp/resources/views/Order/Show.blade.php
+++ NavyFoodApp/resources/views/Order/Show.blade.php
@@ -132,7 +132,7 @@
                             </div>
 
                         </div>
-                        <a href="{{route('order.edit', $Order)}}" style="align-self: center; background: lightsteelblue">Edit</a>
+                        <a href="{{route('order.edit', $Order)}}" style="align-items: center; background: lightsteelblue">Edit</a>
                     </div>
                 </div>
 
