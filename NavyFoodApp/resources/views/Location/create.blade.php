008bc34450a2f999d5929630fd4c235e07922d71
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-11-05 11:21:53 +0800

    formatting

--- NavyFoodApp/resources/views/Location/create.blade.php
+++ NavyFoodApp/resources/views/Location/create.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-            <form method="POST" action="{{route('location.store')}}">
+            <form method="POST" action="{{route('location.store')}}" style="align-content: center">
                 @csrf
 
                 <div style="text-align: left; padding-bottom:4px;">
