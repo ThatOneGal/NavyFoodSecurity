0eae6d3b19851a160b1edca64ac40d15a9ecd3e0
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-11-05 12:40:33 +0800

    Update Show.blade.php

--- NavyFoodApp/resources/views/Order/Show.blade.php
+++ NavyFoodApp/resources/views/Order/Show.blade.php
@@ -1,36 +1,39 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Order:') }} {{$Order->id}}
+            {{ __('Order:') }} {{$OrderNum->id}}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="display:flex; align-items: center; justify-content: center">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                <div> {{--Destination--}}
-                    <div><label for="Location" style="font-weight: bold">Destination:</label></div>
-                    <div>
+                <form style="text-align: center">
+                    @csrf
+
+                    <div> {{--Destination--}}
+                        <div><label for="Location">Destination:</label></div>
                         <div>
-                            <label for="Location">
-                                @foreach($locationList as $location)
-                                    @if ($Order->LocationId == $location->id)
-                                        {{$location->locationName}}
-                                    @endif
-                                @endforeach
-                            </label>
+                            <div>
+                                <label for="Location">
+                                    @foreach($locationList as $location)
+                                        @if ($OrderNum->LocationId == $location->id)
+                                            {{$location->locationName}}
+                                        @endif
+                                    @endforeach
+                                </label>
+                            </div>
                         </div>
                     </div>
 
-
                     <div> {{--Status--}}
-                        <div><label for="Status" style="font-weight: bold">Status:</label></div>
+                        <div><label for="Status">Status:</label></div>
 
                         <div>
                             <label for="Status">
                                 @foreach($statusList as $status)
-                                    @if ($Order->StatusId == $status->id)
+                                    @if ($OrderNum->StatusId == $status->id)
                                         {{$status->statusName}}
                                     @endif
                                 @endforeach
@@ -38,113 +41,111 @@
                         </div>
                     </div>
 
-
                     <div> {{--Order Date--}}
                         <div>
-                            <label for="OrderDate" style="font-weight: bold">Date Ordered:</label>
+                            <label for="OrderDate">Date Ordered:</label>
                         </div>
                         <div>
-                            <label for="OrderDate">{{$Order->OrderDate}}</label>
+                            <label for="OrderDate">{{$OrderNum->OrderDate}}</label>
                         </div>
                     </div>
 
                     <div> {{--Order Shipped--}}
                         <div>
-                            <label for="OrderShipped" style="font-weight: bold">Date Shipped:</label>
+                            <label for="OrderShipped">Date Shipped:</label>
                         </div>
                         <div>
                             <label for="OrderShipped">
-                                @if($Order->OrderShipped == null)
+                                @if($OrderNum->OrderShipped == null)
                                     0000-00-00 00:00:00
                                 @else
-                                    {{$Order->OrderShipped}}
+                                    {{$OrderNum->OrderShipped}}
                                 @endif </label>
                         </div>
                     </div>
 
                     <div> {{--Order Packed--}}
                         <div>
-                            <label for="OrderPacked" style="font-weight: bold">Date Packed:</label>
+                            <label for="OrderPacked">Date Packed:</label>
                         </div>
                         <div>
                             <label for="OrderPacked">
-                                @if($Order->OrderPacked == null)
+                                @if($OrderNum->OrderPacked == null)
                                     0000-00-00 00:00:00
                                 @else
-                                    {{$Order->OrderPacked}}
+                                    {{$OrderNum->OrderPacked}}
                                 @endif
                             </label>
                         </div>
                     </div>
 
-
                     <div> {{--Packaged Qty--}}
                         <div>
-                            <label for="PackageQty" style="font-weight: bold">Package Qty:</label>
+                            <label for="PackageQty">Package Qty:</label>
                         </div>
                         <div>
                             <textarea name="PackageQty" id="PackageQty" cols="0"
-                                      rows="0" readonly>{{$Order->PackageQty}}</textarea>
+                                      rows="0" readonly>{{$OrderNum->PackageQty}}</textarea>
                         </div>
 
                     </div>
 
                     <div> {{--Order Name--}}
                         <div>
-                            <label for="CustomerId" style="font-weight: bold">Order Recipient</label>
+                            <label for="CustomerId">Order Recipient</label>
                         </div>
 
                         <div>
                             <input type="text" id="CustomerId"
-                                   value="{{$Order->CustomerId }} {{--Variable for users details--}} " readonly>
+                                   value="{{$OrderNUm->CustomerId }} {{--Variable for users details--}} " readonly>
                         </div>
                     </div>
 
-
                     <div>{{--Order Content--}}
                         <div>
-                            <label for="Content" style="font-weight: bold">Content:</label>
+                            <label for="Content">Content:</label>
                         </div>
 
                         <div>
                             <textarea name="Content" id="Content" cols="0" rows="0"
-                                      readonly>{{$Order->Content}}</textarea>
+                                      readonly>{{$OrderNum->Content}}</textarea>
                         </div>
                     </div>
 
                     <div> {{--Notes Storage--}}
                         <div>
-                            <label for="NotesStorage" style="font-weight: bold">Notes:</label>
+                            <label for="NotesStorage">Notes:</label>
                         </div>
                         <div>
                             <textarea name="NotesStorage" id="NotesStorage" cols="0"
-                                      rows="0" readonly>{{$Order->NotesStorage}}</textarea>
+                                      rows="0" readonly>{{$OrderNum->NotesStorage}}</textarea>
                         </div>
-
-
-                        <div> {{--Notes Preparation--}}
-                            <div>
-                                <label for="NotesPreparation" style="font-weight: bold">Notes Preparation :</label>
-                            </div>
-                            <div>
-                            <textarea name="NotesPreparation" id="NotesPreparation" cols="0"
-                                      rows="0" readonly>{{$Order->NotesPreparation}}</textarea>
-                            </div>
-
-                        </div>
-                        <a href="{{route('order.edit', $Order)}}" style="background: #efefef; border: black">Edit</a>
                     </div>
-                </div>
 
-                <div style="margin-left: 200px">{{--Display QR Code--}}
-                    {!! QrCode::size(500)->format('svg')->generate($Order, public_path('images/qrcode.svg')) !!}
-                    <img src="{{url('/images/qrcode.svg')}}"/>
+                    <div> {{--Notes Preparation--}}
+                        <div>
+                            <label for="NotesPreparation">Notes Preparation:</label>
+                        </div>
+                        <div>
+                            <textarea name="NotesPreparation" id="NotesPreparation" cols="0"
+                                      rows="0" readonly>{{$OrderNum->NotesPreparation}}</textarea>
+                        </div>
+                    </div>
 
-
-                </div>
-
+                    <div style="margin-top: 10px">
+                        <a href="{{route('order.edit', $OrderNum)}}">Edit</a>
+                    </div>
+                </form>
             </div>
         </div>
+
+        <div>
+            {!! QrCode::size(250)->format('svg')->generate($OrderNum, public_path('images/qrcode.svg')) !!}
+            <img src="{{url('/images/qrcode.svg')}}"/>
+        </div>
+
+    </div>
+    </div>
     </div>
 
 </x-app-layout>
