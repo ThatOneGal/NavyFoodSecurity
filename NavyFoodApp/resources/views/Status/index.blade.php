7955db39b0dc218194ee9006bc5674b790ebea7d
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-11-07 12:44:50 +0800

    Update index.blade.php

--- NavyFoodApp/resources/views/Status/index.blade.php
+++ NavyFoodApp/resources/views/Status/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                <table class="mx-auto" style="text-align: center; mso-cellspacing: 20px">
+                <table class="mx-auto">
                     {{--Table header--}}
                     <thead>
                     <tr>
