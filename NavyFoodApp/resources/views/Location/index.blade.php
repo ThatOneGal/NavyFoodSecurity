5b3548193792648566eda5a4abea643dd5b6ad4d
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2021-11-07 13:15:30 +0800

    margins

--- NavyFoodApp/resources/views/Location/index.blade.php
+++ NavyFoodApp/resources/views/Location/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                <table class="mx-auto" cellpadding="15" cellspacing="15" style="margin-top: 10px; margin-bottom: 10px">
+                <table class="mx-auto" cellpadding="15" cellspacing="15" style="margin-top: 10px; margin-bottom: 4px">
                     {{--Table header--}}
                     <thead>
                     <tr>
@@ -41,7 +41,7 @@
                     </tbody>
                 </table>
 
-                <div style="text-align: center; margin-top: 10px">
+                <div style="text-align: center; margin-top: 10px; margin-bottom: 10px">
                     <a href="{{route('location.create')}}" style="background: #efefef; border: black;"
                         class="button">Create New</a>
                 </div>
