0e39b25682514da73da3b3a6056ec05fcbbe5d44
William <69453507+BoxedSnake@users.noreply.github.com>
2021-11-17 21:44:15 +0800

    bring up to main

---
+++ NFA/NFA/NFA/Models/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFA.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string rank { get; set; }
+        public string serialnumber { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int UserRoleId { get; set; }
+
+
+
+        public User()
+        {
+        }
+
+    } // class
+} // namespace
+
+/*
+public DateTime OrderPacked
+{
+    get { return _OrderPacked; }
+    set
+    {
+        if (value == null)
+        {
+            _OrderPacked = DateTime.Now;
+        }
+        else
+        {
+            _OrderPacked = value;
+        }
+    }
+}
+public string PackageQty { get; set; }
+
+public int PackedId
+{
+    get { return _PackerId; }
+    set
+    {
+        if (value == null)
+        {
+            _PackerId = 0;
+        }
+        else
+        {
+            _PackerId = value;
+        }
+    }
+}
+
+*/
